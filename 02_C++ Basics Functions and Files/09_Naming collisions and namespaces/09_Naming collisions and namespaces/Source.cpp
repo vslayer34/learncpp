a34afb9744a33a564726605805a88bf502381cac
vslayer34 <mohamd.elnagar.34@gmail.com>
2022-11-17 05:41:04 +0200

    09_Naming collisions and namespaces

---
+++ 02_C++ Basics Functions and Files/09_Naming collisions and namespaces/09_Naming collisions and namespaces/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;			// this is a using directive that allows us to access names in the std namespace with no namespace prefix
+
+int main()
+{
+	cout << "Hello, World!!!" << endl;
+	
+	return 0;
+}
