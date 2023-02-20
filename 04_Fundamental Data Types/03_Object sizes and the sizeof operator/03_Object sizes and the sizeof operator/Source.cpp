f5c83de1715a3c13be03931af70900face1983ef
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-02-20 09:39:24 +0200

    finished 4.3 — Object sizes and the sizeof operator

--- 04_Fundamental Data Types/03_Object sizes and the sizeof operator/03_Object sizes and the sizeof operator/Source.cpp
+++ 04_Fundamental Data Types/03_Object sizes and the sizeof operator/03_Object sizes and the sizeof operator/Source.cpp
@@ -2,7 +2,26 @@
 
 int main()
 {
-	std::cout << "Hey Dell G5" << std::endl;
-	
-	return 0;
+    std::cout << "bool:\t\t" << sizeof(bool) << " bytes\n";
+    std::cout << "char:\t\t" << sizeof(char) << " bytes\n";
+    std::cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes\n";
+    std::cout << "char16_t:\t" << sizeof(char16_t) << " bytes\n";
+    std::cout << "char32_t:\t" << sizeof(char32_t) << " bytes\n";
+    std::cout << "short:\t\t" << sizeof(short) << " bytes\n";
+    std::cout << "int:\t\t" << sizeof(int) << " bytes\n";
+    std::cout << "long:\t\t" << sizeof(long) << " bytes\n";
+    std::cout << "long long:\t" << sizeof(long long) << " bytes\n";
+    std::cout << "float:\t\t" << sizeof(float) << " bytes\n";
+    std::cout << "double:\t\t" << sizeof(double) << " bytes\n";
+    std::cout << "long double:\t" << sizeof(long double) << " bytes\n";
+
+    std::cout << "=============================================" << "\n";
+    std::cout << "=============================================" << "\n";
+
+    std::cout << "You can also use the sizeof operator on a variable name:" << "\n";
+
+    int x{  };
+    std::cout << "x is: " << sizeof(x) << " bytes\n";
+
+    return 0;
 }
