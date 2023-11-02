05bf87bc8fc0165ec01fd51e555f3b4b0c3cd793
vslayer34 <99190012+vslayer34@users.noreply.github.com>
2023-11-02 16:20:21 +0200

    12.7 — Introduction to pointers
    https://www.learncpp.com/cpp-tutorial/introduction-to-pointers/

--- 12_Compound Types. References and Pointers/07_Introduction to pointers/sourve.cpp
+++ 12_Compound Types. References and Pointers/07_Introduction to pointers/sourve.cpp
@@ -78,5 +78,168 @@ int main()
     }
     std::cout << std::string(70, '-') << '\n';
 
+    //---------------------------------------------------------------------------------------------
+
+    // Pointers and assignment
     
+    // We can use assignment with pointers in two different ways:
+    // // To change what the pointer is pointing at (by assigning the pointer a new address)
+    // // To change the value being pointed at (by assigning the dereferenced pointer a new value)
+    
+    // First, let’s look at a case where a pointer is changed to point at a different object:
+    {
+        int x { 5 };
+        int* ptr { &x };                        // ptr initialized to point at x
+
+        std::cout << "Pointer to x' address: " << *ptr << '\n';              // print the value at the address being pointed to (x's address)
+
+        int y { 6 };
+        ptr = &y;                               // change the pointer to point ay y
+
+        std::cout << "Pointer to y' address: " << *ptr << '\n';              // print the value at the address being pointed to (y's address)
+    }
+    std::cout << std::string(70, '-') << '\n';
+
+    // In the above example, we define pointer ptr, initialize it with the address of x, and dereference the pointer to print the value being pointed to (5).
+    // We then use the assignment operator to change the address that ptr is holding to the address of y.
+    // We then dereference the pointer again to print the value being pointed to (which is now 6).
+
+    // Now let’s look at how we can also use a pointer to change the value being pointed at:
+    {
+        int x { 5 };
+        int* ptr { &x };                    // initialize the pointer with the address of variable x
+
+        std::cout << "x's value: " << x << '\n';
+        std::cout << "The value at the address being pointed to (x's address): " << *ptr << '\n';
+
+        std::cout << "Changeing the the value at the address the pointer is pointing at." << '\n';
+        std::cout << "...." << '\n';
+        std::cout << "....." << '\n';
+
+        *ptr = 6;
+
+        std::cout << "new x's value: " << x << '\n';
+        std::cout << "The value at the address being pointed to (x's address): " << *ptr << '\n';
+    }
+    std::cout << std::string(70, '-') << '\n';
+
+    // In this example, we define pointer ptr, initialize it with the address of x, and then print the value of both x and *ptr (5).
+    // Because *ptr returns an lvalue, we can use this on the left hand side of an assignment statement, which we do to change the value being pointed at by ptr to 6.
+    // We then print the value of both x and *ptr again to show that the value has been updated as expected.
+
+    //---------------------------------------------------------------------------------------------
+
+    // Pointers behave much like lvalue references
+
+    // Pointers and lvalue references behave similarly. Consider the following program:
+    {
+        int x { 5 };
+        int& ref { x };                                              // get a reference to x
+        int* ptr { &x };                                             // get a pointer to x address
+
+        std::cout << "x: " << x << '\n';
+        std::cout << "ref: " << ref << '\n';                        // we use reference to print the value of x
+        std::cout << "*ptr: " << *ptr << '\n' << '\n';              // use the pointer to print the value at x's memory address
+
+        ref = 6;                                                    // use the reference to change the value of x
+        
+        std::cout << "x: " << x << '\n';           
+        std::cout << "ref: " << ref << '\n';                        // use the reference to print the new value of x
+        std::cout << "*ptr: " << *ptr << '\n' << '\n';              // use the pointer to print the new value of x
+
+        *ptr = 7;                                                   // use the pointer to change the value of x
+
+        std::cout << "x: " << x << '\n';
+        std::cout << "ref: " << ref << '\n';                        // use the reference to print the value of x
+        std::cout << "*ptr: " << *ptr << '\n';                     // use the pointer to print the new value of x
+    }
+    std::cout << std::string(70, '-') << '\n';
+
+    //---------------------------------------------------------------------------------------------
+
+    // The size of pointers
+
+    // The size of a pointer is dependent upon the architecture the executable is compiled for -- a 32-bit executable uses 32-bit memory addresses -- consequently,
+    // a pointer on a 32-bit machine is 32 bits (4 bytes).
+    // With a 64-bit executable, a pointer would be 64 bits (8 bytes).
+    // Note that this is true regardless of the size of the object being pointed to:
+    {
+        char* charPtr { };                                  // chars are 1 byte
+        int* intPtr { };                                    // ints are usually 4 bytes
+        long double longDoublePtr { };                      // long doubles are usually 8 to 12 bytes
+
+        std::cout << sizeof(charPtr) << '\n';               // prints 4
+        std::cout << sizeof(intPtr) << '\n';                // print 4 also
+        std::cout << sizeof(longDoublePtr) << '\n';         // prints 4 too
+    }
+    std::cout << std::string(70, '-') << '\n';
+
+    // The size of the pointer is always the same.
+    // This is because a pointer is just a memory address, and the number of bits needed to access a memory address is constant.
+
+    //---------------------------------------------------------------------------------------------
+
+    // Dangling pointers
+    // Here’s an example of creating a dangling pointer:
+    {
+        int x { 5 };
+        int* ptr { &x };
+
+        std::cout << *ptr << '\n';                  // is valid
+        {
+            int y { 6 };
+            ptr = &y;
+
+            std::cout << *ptr << '\n';              // also is valid
+        }
+        // y goes out of the scope, and now it's a dangling probliem
+
+        std::cout << *ptr << '\n';                  // undefined behaviour from dereferencing a dangling pointer
+    }
+    std::cout << std::string(70, '-') << '\n';
+
+    // The above program will probably print:
+    // 5
+    // 6
+    // 6
+    // But it may not, as the object that ptr was pointing at went out of scope and was destroyed at the end of the inner block, leaving ptr dangling.
+
+    //---------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------
+
+    // Quiz 
+    // Question #1
+    // What values does this program print? Assume a short is 2 bytes, and a 32-bit machine.
+    {
+        short value { 7 };                                  // &value: 0012FF60
+        short otherValue { 3 };                             // &otherValue: 0012FF54
+
+        short* ptr { &value };
+
+        std::cout << &value << '\n';                        // 0012FF60
+        std::cout << value << '\n';                         // 7
+        std::cout << ptr << '\n';                           // 0012FF60
+        std::cout << *ptr << '\n';                          // 7
+        std::cout << '\n';
+
+        *ptr = 9;
+
+        std::cout << &value << '\n';                        // 0012FF60
+        std::cout << value << '\n';                         // 9
+        std::cout << ptr << '\n';                           // 0012FF60
+        std::cout << *ptr << '\n';                          // 9
+        std::cout << '\n';
+
+        ptr = &otherValue;
+
+        std::cout << &otherValue << '\n';                   // 0012FF54
+        std::cout << otherValue << '\n';                    // 3
+        std::cout << ptr << '\n';                           // 0012FF54
+        std::cout << *ptr << '\n';                          // 3
+        std::cout << '\n';
+
+        std::cout << sizeof(ptr) << '\n';                   // 4
+        std::cout << sizeof(*ptr) << '\n';                  // 2
+    }
 }
